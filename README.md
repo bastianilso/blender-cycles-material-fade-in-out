c613a76bc2ab75ed1f9a799aa656034ee4b6f901
Bastian Ilsø <bastianilso@gnome.org>
2017-06-10 00:45:36 +0200

    Material Tools v0.1: Basic Fade-in/out operations
    Adds two operations "Fade-in" and "Fade-out". They
    can create fades and make adjustments to existing
    fade setups (however, they currently dont clear
    previous keyframes).

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# blender-cycles-material-fade-in-out
+# Material Fade Tools
 Blender plug-in for creating fade animations on multiple objects in cycles
+
+![The addon creates fade-in and fade-out effects on all selected objects](http://bastianilso.com/wp-content/uploads/2017/06/select_by_name.png)
+
+Adds a menu in the 'Commotion' tab of the T-Panel, with functionality to fade each selected object between complete transparency and full opaqueness for a customizable period of time.
+
+## How to install
+
+ 1. Download a ZIP of this repository
+ 2. Open Blender. Go to _File_ ➙ _User Preferences_ ➙ _Addons_ ➙ _Install from ZIP_ and choose the downloaded ZIP folder.
+ 3. Activate the Plug-in by clicking the checkmark next to "Material Fade Tools"
+4. Press _Save User Settings_ and close the window.
+ 
+## How to use
+
+After following installation instructions, a _Commotion_ tab will be available in the Tool panel (activated by pressing _T_), with a _Material Tools_ panel. It's only visible when you have objects selected which have material applied to them.
+
+Licensed under GPL 3.0
---
+++ __init__.py
@@ -0,0 +1,189 @@
+bl_info = {  
+ "name": "Material Fade Tools",  
+ "author": "Bastian Ilso (bastianilso)",  
+ "version": (0, 1),  
+ "blender": (2, 7, 8),  
+ "location": "Tools Panel -> Commotion",  
+ "description": "Tools for creating fades and other effects on materials.",  
+ "warning": "",
+ "wiki_url": "",  
+ "tracker_url": "https://github.com/bastianilso/blender-cycles-material-fade-in-out",  
+ "category": "Material"}
+
+import bpy
+from bpy.types import Panel, Operator, PropertyGroup
+from bpy.props import IntProperty, PointerProperty
+
+class UI:
+    bl_category = 'Commotion'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_context = 'objectmode'
+
+    @classmethod
+    def poll(cls, context):
+        ob = context.active_object
+        return ob is not None and len(ob.material_slots) > 0 and bpy.context.scene.render.engine == 'CYCLES'
+
+class MaterialToolProperties(PropertyGroup):
+    duration = IntProperty(
+            name="Duration",
+            description="Duration of the animation (in frames)",
+            default=13,
+            subtype="TIME",
+            )
+
+class MaterialTools(UI,Panel):
+    """Creates a material tool panel in the commotion tab"""
+    bl_label = 'Material Tools'
+    bl_idname = 'commotion_mat_tools'
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        layout.prop(scene.commotion_mat_tools, "duration")
+        layout.operator("object.fade_in", icon="MATERIAL", text="Fade In")
+        layout.operator("object.fade_out", icon="MATERIAL", text="Fade Out")
+
+class NodeUtils:
+    
+    def create_fade_nodes(self, material):
+        tree = material.node_tree
+        trans_node = tree.nodes.new(type='ShaderNodeBsdfTransparent')
+        mix_node = tree.nodes.new(type='ShaderNodeMixShader')
+        
+        inp = mix_node.inputs[1]
+        out = trans_node.outputs['BSDF']
+        tree.links.new(inp,out)
+
+        if not tree.nodes['Material Output']:
+            tree.nodes.new(type='ShaderNodeOutputMaterial')
+
+        if tree.nodes['Material Output'].inputs['Surface'].links:
+            inp = mix_node.inputs[2]
+            out = tree.nodes['Material Output'].inputs['Surface'].links[0].from_node.outputs[0]
+            tree.links.new(inp,out)
+        
+        inp = tree.nodes['Material Output'].inputs['Surface']
+        out = mix_node.outputs['Shader']
+        tree.links.new(inp,out)
+        
+        return mix_node
+    
+    def detect_fade_nodes(self, material):
+        tree = material.node_tree
+        
+        if not tree.nodes['Material Output']:
+            #print("No Material Output")
+            return None 
+        
+        if not tree.nodes['Material Output'].inputs['Surface'].links:
+            #print("Nothing linked with Material Output")
+            return None
+        
+        from_node = tree.nodes['Material Output'].inputs['Surface'].links[0].from_node
+        
+        if not 'Mix Shader' in from_node.name:
+            #print("Mix Shader not connected to Material Output")
+            return None
+        
+        if not from_node.inputs[1].links:
+            #print("Mix Shader has no inputs")
+            return None
+        
+        if not 'Transparent BSDF' in from_node.inputs[1].links[0].from_node.name:
+            #print("Mix Shader does not have Trans BSDF as first shader input")
+            return None
+        
+        if from_node.inputs['Fac'].links:
+            #print("Mix Shader's Fac is connected")
+            return None        
+        
+        return from_node
+
+class FadeOut(Operator):
+    """Fade out a material"""
+    bl_idname = "object.fade_out"
+    bl_label = "Fade Out"
+    def execute(self,context):
+        duration = bpy.context.scene.commotion_mat_tools.duration
+        nodeutils = NodeUtils()
+        
+        for ob in bpy.context.selected_objects:
+            for slot in ob.material_slots:
+                if not slot.material:
+                    continue
+                
+                slot.material.use_nodes = True
+                
+                mix_node = nodeutils.detect_fade_nodes(slot.material)
+        
+                if mix_node is None:
+                    mix_node = nodeutils.create_fade_nodes(slot.material)
+
+                # Delete old keyframes if any
+                #mix_node.inputs['Fac'].id_data.animation_data_clear()
+                
+                # Animate Factor on Mix Shader
+                mix_node.inputs['Fac'].default_value = 1.0
+                mix_node.inputs['Fac'].keyframe_insert(data_path="default_value", index=-1)
+                bpy.context.scene.frame_current += duration
+                mix_node.inputs['Fac'].default_value = 0.0
+                mix_node.inputs['Fac'].keyframe_insert(data_path="default_value", index=-1)
+                bpy.context.scene.frame_current -= duration          
+                
+        return {'FINISHED'}
+
+
+class FadeIn(Operator):
+    """Fade in a material"""
+    bl_idname = "object.fade_in"
+    bl_label = "Fade In"
+    def execute(self,context):
+        duration = bpy.context.scene.commotion_mat_tools.duration
+        nodeutils = NodeUtils()
+        
+        for ob in bpy.context.selected_objects:
+            for slot in ob.material_slots:
+                if not slot.material:
+                    continue
+                
+                slot.material.use_nodes = True
+                
+                mix_node = nodeutils.detect_fade_nodes(slot.material)
+        
+                if mix_node is None:
+                    mix_node = nodeutils.create_fade_nodes(slot.material)
+
+                # Delete old keyframes if any
+                #mix_node.inputs['Fac'].id_data.animation_data_clear()
+                
+                # Animate Factor on Mix Shader
+                mix_node.inputs['Fac'].default_value = 0.0
+                mix_node.inputs['Fac'].keyframe_insert(data_path="default_value", index=-1)
+                bpy.context.scene.frame_current += duration
+                mix_node.inputs['Fac'].default_value = 1.0
+                mix_node.inputs['Fac'].keyframe_insert(data_path="default_value", index=-1)
+                bpy.context.scene.frame_current -= duration          
+                
+        return {'FINISHED'}
+
+classes = (
+    MaterialTools,
+    FadeIn,
+    FadeOut,
+    MaterialToolProperties,
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.commotion_mat_tools = PointerProperty(type=MaterialToolProperties)
+
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+if __name__ == "__main__":
+    register()
